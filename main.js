d2853b2daf6d3528c16a099b751efe6693a87a2d
Hylje500001~ <miska.kyto@gmail.com>
2022-07-26 15:10:05 +0300

    Korttidesign, tallennus pitäisi toimia?

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let kuormaaja = [];
 let kaikki = [];
+let labels = ['ID: ', 'Nimi: ', 'Mallikuvaus: ', 'Kuivapaino: ', 'Märkapaino: ', 'Valmistuslinja: ', 'Valmistus aloitettu: ', 'Valmistus lopetettu: ', 'Viimeistelty: '];
 
 
 function Kuormaaja() {
@@ -25,32 +26,25 @@ function Kuormaaja() {
     kaikki.push(kuormaaja);
     console.log(kaikki)
 
-    let table = document.getElementById('table');
+    
+    // render kuormaaja to card
+    let card = document.getElementById('kortit');
+    let newCard = document.createElement('div');
+    newCard.className = "card mb-3";
+    newCard.style = "width: 18rem;";
+    newCard.appendChild(document.createElement('div'));
 
-    let row = table.insertRow(0)
-
-    //clear table
-    while(table.rows.length > 1) {
-        table.deleteRow(1);
-    }
-
-    kaikki.forEach(function(row) {
-        let newRow = document.createElement('tr');
-        table.appendChild(newRow);
-
-        if(row instanceof Array) {
-            row.forEach(function(cell) {
-                let newCell = document.createElement('td');
-                newCell.innerHTML = cell;
-                newRow.appendChild(newCell);
-                });
-            }
-        else {
-            newCell = document.createElement('td');
-            newCell.innerHTML = row;
-            newRow.appendChild(newCell);
+    newCard.appendChild(document.createElement('ul'));
+    newCard.childNodes[1].className = "list-group list-group-flush";
+    for(let i = 0; i < kuormaaja.length; i++){
+        newCard.childNodes[1].appendChild(document.createElement('li'));
+        newCard.childNodes[1].childNodes[i].className = "list-group-item";
+        newCard.childNodes[1].childNodes[i].innerHTML += labels[i] + kuormaaja[i] + " ";
         }
-    });
+    card.appendChild(newCard);
+
+    // save card to localStorage
+    localStorage.setItem('kuormaajat', JSON.stringify(kaikki));
 
     document.getElementById('ID').value = "";
     document.getElementById('title').value = "";
@@ -62,40 +56,33 @@ function Kuormaaja() {
     document.getElementById('lopetus').value = "";
     document.getElementById('viimeistely').checked = false;
 
-    // tallenetaan taulukko localStorageen
-    localStorage.setItem('kuormaajat', JSON.stringify(kaikki));
-
 }
 
 function Load() {
     // get data from localStorage
     let data = JSON.parse(localStorage.getItem('kuormaajat'));
-    // map data to table
-    let table = document.getElementById('table');
+    for(let i = 0; i < data.length; i++){
+        kuormaaja = data[i];
 
-    // if rows in table, delete them
-    while(table.rows.length >= 1) {
-        table.deleteRow(0);
+        let card = document.getElementById('kortit');
+        for(let x = 0; x < data.length-1; x++){
+            let newCard = document.createElement('div');
+            newCard.className = "card mb-3";
+            newCard.style = "width: 18rem;";
+            newCard.appendChild(document.createElement('div'));
+
+            newCard.appendChild(document.createElement('ul'));
+            newCard.childNodes[1].className = "list-group list-group-flush";
+            for(let y = 0; y < kuormaaja.length; y++){
+                newCard.childNodes[1].appendChild(document.createElement('li'));
+                newCard.childNodes[1].childNodes[y].className = "list-group-item";
+                newCard.childNodes[1].childNodes[y].innerHTML += labels[y] + kuormaaja[y] + " ";
+                }
+        card.appendChild(newCard);
+    }
     }
 
-
-    data.forEach(function(row) {
-        let newRow = document.createElement('tr');
-        table.appendChild(newRow);
-
-        if(row instanceof Array) {
-            row.forEach(function(cell) {
-                let newCell = document.createElement('td');
-                newCell.innerHTML = cell;
-                newRow.appendChild(newCell);
-                });
-            }
-        else {
-            newCell = document.createElement('td');
-            newCell.innerHTML = row;
-            newRow.appendChild(newCell);
-        }
-    });
+    
 }
 
 
